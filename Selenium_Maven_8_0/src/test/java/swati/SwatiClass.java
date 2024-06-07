36f3e6fc99ceeb2f7f129f20cca9dc8d11866020
suchi <suchi@SUCHITRA>
2024-06-07 09:28:05 +0530

    swati class added

---
+++ Selenium_Maven_8_0/src/test/java/swati/SwatiClass.java
@@ -0,0 +1,13 @@
+package swati;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SwatiClass 
+{
+	@Test
+	public void test() {
+		Reporter.log("Swati class has added",true);
+	}
+
+}

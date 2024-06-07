6ed7b2d4504edd715fb4bf44fb4212ed5bf28fec
hp <hp@Alka>
2024-06-07 09:35:32 +0530

    AlkaClass added

---
+++ Selenium_Maven_8_0/src/test/java/Alka/AlkaClass.java
@@ -0,0 +1,13 @@
+package Alka;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AlkaClass 
+{@Test
+	public void test()
+	{
+	Reporter.log("AlkaClass added",true);
+	}
+
+}
